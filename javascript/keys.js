e703d0979366668ae223a2341ab09610cd2eed81
Caitlin Barron <barroncn@gmail.com>
2017-11-28 13:58:46 +0000

    Initial Files

---
+++ javascript/keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'RWrzO7JPFT288lhsKth0L0iAO',
+  consumer_secret: 'l3erLKWAYwH3gStFk6S4o8W9wpLLUoz7eDIJ0LKFHhw8KwdAn7',
+  access_token_key: '935488289741688832-t1OB99s3mwLe8gRiME9HzZTUYAEhCgO',
+  access_token_secret: '8MRugb2lTvd5DYUdKOcEvPHBShlcWBdVQqTlyDq0ucH2S'
+};
+
+module.exports = twitterKeys;
+
+
+//Spotify Client ID: d8e9a27c345a41ba9c535f9339f69d58
+//Spotify Client Secret: 55c09360dba748dc8d7a66bb1c582a72
